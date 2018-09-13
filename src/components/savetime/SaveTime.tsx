612a0f3bf0595308584e329b114e3f9e9940f16a
DevonPeroutky <devonperoutky@gmail.com>
2018-09-14 00:33:05 +0800

    New badge

--- src/components/savetime/SaveTime.tsx
+++ src/components/savetime/SaveTime.tsx
@@ -1,5 +1,6 @@
 import * as React from "react";
 import "./savetime.css";
+import TWJBadge from "../../assets/TWJbadge.png";
 import {Icon, IconName} from "@blueprintjs/core";
 import {Constants} from "../../Constants";
 import ScrollableAnchor from "react-scrollable-anchor";
@@ -9,7 +10,7 @@ export const SaveTime = (props: {}) => (
     <ScrollableAnchor id={Constants.SAVE_TIME}><div className="title">Have a great vacation without the hassle of planning!</div></ScrollableAnchor>
     <div className="icon-row">
       <IconItem icon="globe" title="Expert advice without the cost" description="There’s no cost. Our commission comes from our suppliers, so booking with us means you get top-notch service for free."/>
-      <IconItem icon="badge" title="We are certified in special needs travel!" description="We are trained and certified to best prepare for those needing special diets, on the autism spectrum, needing low-sensory environments, in a wheelchair, with a service dog, or needing allergy friendly accommodations." />
+      <BadgeItem title="We are certified in special needs travel!" description="We are trained and certified to best prepare for those needing special diets, on the autism spectrum, needing low-sensory environments, in a wheelchair, with a service dog, or needing allergy friendly accommodations." />
       <IconItem icon="chat" title="We tailor your experience!" description="You are not just a name and a number to us. Every client will plan their travel with a certified travel agent over phone, email, or text." />
     </div>
   </div>
@@ -23,3 +24,10 @@ const IconItem = (props: {title: string, description: string, icon: IconName}) =
   </div>
 )
 
+const BadgeItem = (props: {title: string, description: string}) => (
+  <div className="icon-item">
+    <img src={TWJBadge} height="100" width="100"/>
+    <div className="i-title">{props.title}</div>
+    <div className="description">{props.description}</div>
+  </div>
+)
