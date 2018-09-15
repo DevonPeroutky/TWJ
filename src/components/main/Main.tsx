65cce22cbe2aea42157fe44578dd311bc00d0efe
DevonPeroutky <devonperoutky@gmail.com>
2018-09-15 10:08:36 +0800

    New logo

--- src/components/main/Main.tsx
+++ src/components/main/Main.tsx
@@ -1,5 +1,5 @@
 import * as React from "react";
-import TWJLogo from "../../assets/twj.png";
+import TWJLogo from "../../assets/new_logo.png";
 import {Icon} from "@blueprintjs/core";
 import "./main.css";
 import {Constants} from "../../Constants";
@@ -7,7 +7,7 @@ import {Constants} from "../../Constants";
 export const Main = () => (
   <div className="main-container">
     <div className="header">
-      <div className="landria-label"><img className="logo" src={TWJLogo}/>TrippinWithJamie</div>
+      <div><img className="logo" src={TWJLogo} height="150" width="200"/></div>
     </div>
     <div className="content">
       <div className="title">Where to next?</div>
@@ -15,4 +15,3 @@ export const Main = () => (
     <div className="footer"><a href={"#" + Constants.SAVE_TIME}><Icon icon="chevron-down" iconSize={60}/></a></div>
   </div>
 );
-
